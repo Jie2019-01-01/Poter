38096da3c8a6c67f0f70525b986680e81b2ae7ea
Jie2019-01-01 <wy12012156@163.com>
2019-03-13 14:09:50 +0800

    46546465456

--- porter_lightweight/file-service/src/main/java/com/huawei/cse/porter/file/service/FileServiceImpl.java
+++ porter_lightweight/file-service/src/main/java/com/huawei/cse/porter/file/service/FileServiceImpl.java
@@ -16,6 +16,15 @@ public class FileServiceImpl implements com.huawei.cse.porter.file.api.FileServi
         return fileService.uploadFile(file);
     }
 
+    /**
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     */
     public boolean deleteFile(String id) {
         String session = ContextUtils.getInvocationContext().getContext("session-info");
         if (session == null) {
